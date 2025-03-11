57acdff570e053f1d0f74f2086718d20453faafc
Mr Right <mr.right@dev.test>
2025-03-11 08:25:10 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : 'dashboard' , component : DashboardComponent , title : 'Dashboard'},
+    { path : 'users' , component : UsersComponent , title : 'Users'},
+    { path : '' , redirectTo : '/dashboard' , pathMatch : 'full'}
+];
